39b8283b351d06b3c7fdf12208e3d7b3eca8306d
hayeongKo <96684524+hayeongKo@users.noreply.github.com>
2022-09-06 00:36:15 +0900

    inital jpa setting & mapping

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")) //다대다관계는 한번에 쓰는 것이 아닌 일대다 다대일 관계로 연결하나 실무상 사용 잘 x
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
